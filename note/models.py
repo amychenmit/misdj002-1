2c0b2a953a0ac7032c82707cad82d6592c0e6bd9
Mark Chen <chenpingling@protonmail.com>
2019-10-04 10:56:05 +0800

    show 我的NOTE

--- note/models.py
+++ note/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Note(models.Model):
     subject = models.CharField(max_length=200)
+    detail = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
